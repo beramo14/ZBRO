6164a42dc9df6bc08824b419d7d049726ed1d074
beramo <aqaq556@naver.com>
2023-06-26 00:24:54 +0900

    test code

---
+++ src/main/java/com/zbro/main/controller/MainController.java
@@ -0,0 +1,40 @@
+package com.zbro.main.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zbro.main.service.MainService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class MainController {
+	
+	@Autowired
+	private MainService mainService;
+	
+	@RequestMapping("/")
+	public String mainPage() {
+		
+		//sysout
+		System.out.println("called index mapper");
+		
+		//Slf4j log
+		log.debug("called index mapper"); //debug level에서 실행시 표시(별도 설정 필요)
+		log.info("called index mapper");
+		log.warn("called index mapper");
+		log.error("called index mapper");
+		
+		//slf4j log 사용시 되도록이면 + 연산자 사용하지 말고 slf4j의 치환문자를 사용하자
+		log.info("Hello {}{}!!!","world"," tester");
+		
+		// 개발할때 잠깐 확인용으로 sysout 사용해도 상관없음
+		// 가능 하면 Slf4j logger사용
+		// logger사용 이유 : 어디서 로그 찍었는지 확인할 수 있음
+		
+		
+		return "index";
+	}
+}
