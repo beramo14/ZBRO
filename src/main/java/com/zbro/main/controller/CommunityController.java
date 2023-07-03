3f05de265852a7c5d21a8d174034fddc330d80ae
mcluru <wrose46@naver.com>
2023-07-03 11:43:48 +0900

    230703-1

---
+++ src/main/java/com/zbro/main/controller/CommunityController.java
@@ -0,0 +1,14 @@
+package com.zbro.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CommunityController {
+	
+	@GetMapping("/post_list")
+	public String postListView() {
+		return "/main/community/post_list";
+	}
+
+}
